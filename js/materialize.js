50b7536141c6087bc270cdb9613fa53a0784a302
tjd03 <44816049+tjd03@users.noreply.github.com>
2020-03-24 18:29:28 -0400

    Upload To Publish Branch
    This is where we took the code from my atom.io repository to Upload To Publish Branch to the main unborn head of the Github Repository

---
+++ js/materialize.js
@@ -0,0 +1,8 @@
+// setup Home materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  //initialize Side Navigation Bar
+  var sideNavBar = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNavBar); 
+
+});
